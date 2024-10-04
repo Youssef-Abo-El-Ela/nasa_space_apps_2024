46e98185021e99286f0bc93e20211f3019d795d2
Youssef-Abo-El-Ela <youssefmm2003@gmail.com>
2024-10-04 20:58:06 +0300

    test deployment

--- src/controllers/soil.js
+++ src/controllers/soil.js
@@ -1,12 +1,14 @@
 const axios = require('axios');
 
-const findSoilProp = async (req , res , next)=>{
+const findSoilProp = async (req, res, next) => {
     const response = await axios.get('http://api.weatherbit.io/v2.0/forecast/agweather?lat=30.027732&lon=31.257143&key=a1dcc63aa48540f1b9a1eca42f177403')
     // console.log(response.data)
-    const requiredData = {soilMoisture: response.data.data[0].v_soilm_0_10cm,
-                          specificHumidity: response.data.data[0].specific_humidity,
-                          soilTemperature: response.data.data[0].soilt_0_10cm}
+    const requiredData = {
+        soilMoisture: response.data.data[0].v_soilm_0_10cm,
+        specificHumidity: response.data.data[0].specific_humidity,
+        soilTemperature: response.data.data[0].soilt_0_10cm
+    }
     console.log(requiredData);
 }
 
-module.exports = {findSoilProp}
+module.exports = { findSoilProp }
